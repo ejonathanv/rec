cf98341b3ec3db85b53fcb855fdc9f8028dbc2a5
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-08-07 17:45:36 -0700

    Se corrije la portada de posts en home

--- resources/views/components/website/post-card.blade.php
+++ resources/views/components/website/post-card.blade.php
@@ -1,6 +1,11 @@
 <a href="{{ route('post', $post->slug) }}" class="block w-full md:w-1/3 bg-white bg-opacity-75 post-card">
+    @if($post->type == 'post')
     <div class="post-cover" style="background-image: url({{ asset('uploads/' . $post->cover) }})">
     </div>
+    @else
+    <div class="post-cover" style="background-image: url({{ asset('img/blog_agro_img.png') }})">
+    </div>
+    @endif
     <div class="p-4">
         <p class="small">
             {{ $post->created_at->format('d M, Y') }} | {{ $post->category ? $post->category->name : 'Sin clasificación' }}
