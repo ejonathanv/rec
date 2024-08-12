450d38617a79c094dd04ecf844e65206d6da7503
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-08-12 16:15:52 -0700

    Fix en cover de posts

--- resources/views/components/website/post-card.blade.php
+++ resources/views/components/website/post-card.blade.php
@@ -1,10 +1,15 @@
 <a href="{{ route('post', $post->slug) }}" class="block w-full md:w-1/3 bg-white bg-opacity-75 post-card">
     @if($post->type == 'post')
-    <div class="post-cover" style="background-image: url({{ asset('uploads/' . $post->cover) }})">
+    <div class="post-cover" style="background-image: url('{{ asset('uploads/' . $post->cover) }}')">
     </div>
     @else
-    <div class="post-cover" style="background-image: url({{ asset('img/blog_agro_img.png') }})">
-    </div>
+        @if($post->cover)
+        <div class="post-cover" style="background-image: url('{{ asset('uploads/' . $post->cover) }}')">
+        </div>
+        @else
+        <div class="post-cover" style="background-image: url('{{ asset('img/blog_agro_img.png') }}')">
+        </div>
+        @endif
     @endif
     <div class="p-4">
         <p class="small">
